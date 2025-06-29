2af7c927dae0172e91463c6b2ae8ebd87e81f451
Perial Dupont Kuaguim N <dperial44@gmail.com>
2025-06-29 13:51:26 +0000

    feat: display the arguments into the string result,they are comming from element table

---
+++ element.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+MAIN_MENU(){
+ARG=$1
+
+if [[ -z $ARG ]]
+then
+  echo "Please provide an element as an argument."
+  exit 1
+fi
+
+RESULT=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number::TEXT='$ARG' OR symbol='$ARG' OR name='$ARG';")
+
+if [[ -z $RESULT ]]
+then
+  echo "Element not found."
+else
+  echo "$RESULT" | while IFS="|" read ATOMIC_NUMBER SYMBOL NAME
+  do
+    echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL)"
+  done
+fi
+}
+MAIN_MENU "$1"
