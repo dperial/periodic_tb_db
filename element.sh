491765131a29ae473022be7de05d4a321774aa47
Perial Dupont Kuaguim N <dperial44@gmail.com>
2025-06-29 14:48:16 +0000

    feat: display the correct dtring form with all the arguments coming from elements and properties tables

--- element.sh
+++ element.sh
@@ -5,22 +5,33 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 MAIN_MENU(){
 ARG=$1
 
-if [[ -z $ARG ]]
+if [[ $ARG ]]
 then
-  echo "Please provide an element as an argument."
-  exit 1
-fi
+  # get the arguments from the table elements
+  RESULT_EL=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number::TEXT='$ARG' OR symbol='$ARG' OR name='$ARG';")
 
-RESULT=$($PSQL "SELECT atomic_number, symbol, name FROM elements WHERE atomic_number::TEXT='$ARG' OR symbol='$ARG' OR name='$ARG';")
+  if [[ -z $RESULT_EL ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    RESULT=""
+    while IFS="|" read ATOMIC_NUMBER SYMBOL NAME
+    do
+        RESULT="The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). "
 
-if [[ -z $RESULT ]]
-then
-  echo "I could not find that element in the database."
+        RESULT_PROP=$($PSQL "SELECT type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER;")
+
+        while IFS="|" read TYPE ATOMIC_MASS MELTING BOILING
+        do
+          RESULT+="It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+        done <<< "$RESULT_PROP"
+
+        echo "$RESULT"
+    done <<< "$RESULT_EL"
+  fi
 else
-  echo "$RESULT" | while IFS="|" read ATOMIC_NUMBER SYMBOL NAME
-  do
-    echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL)"
-  done
+  echo -e "Please provide an element as an argument."
 fi
+
 }
 MAIN_MENU "$1"
